5b2b53acda622573d2f9f7da61c29c119170e81a
zhanggaoqi <zhanggaoqi@aobi.com>
2026-03-23 16:20:55 +0800

    [Branch Release] v2.0.0-dev-1406143 - develop
    - 更新所有 XCFrameworks 到 2.0.0-dev-1406143
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -19,6 +19,9 @@ let package = Package(
         .target(
             name: "AOUnityWebBridgeWrapper",
             dependencies: [
+                .byName(name: "AOUnityWebBridge"),
+                .byName(name: "BTWebViewKit"),
+                .byName(name: "UnityBridge"),
                 .product(name: "BTWebViewKit", package: "BTWebViewKit"),
                 .product(name: "UnityBridge", package: "UnityBridge"),
             ],
@@ -31,5 +34,20 @@ let package = Package(
         ),
         
         // ========== Binary Frameworks ==========
+        .binaryTarget(
+            name: "AOUnityWebBridge",
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/AOUnityWebBridge/2.0.0-dev-1406143/AOUnityWebBridge.xcframework.zip",
+            checksum: "604a050d7b921ee0c2791a8c849f52d125039def1de4526eecad607803830bed"
+        ),
+        .binaryTarget(
+            name: "BTWebViewKit",
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/AOUnityWebBridge/2.0.0-dev-1406143/BTWebViewKit.xcframework.zip",
+            checksum: "6d1f083a5b1999db02e6b131e78756e14914bf0557c767bf1956f6e7a9d9b68e"
+        ),
+        .binaryTarget(
+            name: "UnityBridge",
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/AOUnityWebBridge/2.0.0-dev-1406143/UnityBridge.xcframework.zip",
+            checksum: "e60f4084fb6692673766ab4b9a2e64678449bd39f8647c6ef69c10e9d1e80538"
+        )
     ]
 )
