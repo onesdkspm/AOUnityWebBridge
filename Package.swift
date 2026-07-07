bd4db72fb34c426c3c0530af3ce63b1d71350cec
m2构造机 <zhanggaoqi@aobi.com>
2026-07-07 16:30:33 +0800

    [Branch Release] v2.0.2-dev-1480239 - develop
    - 更新所有 XCFrameworks 到 2.0.2-dev-1480239
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -36,8 +36,8 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "AOUnityWebBridge",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/AOUnityWebBridge/1.1.2-dev-1480212/AOUnityWebBridge.xcframework.zip",
-            checksum: "757e25275cdf054b077b5f57c9ffeec752970d76a2bafbe94c050fbd67180088"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/AOUnityWebBridge/2.0.2-dev-1480239/AOUnityWebBridge.xcframework.zip",
+            checksum: "d9a95cb869b8506a7e300cbc38b698735372a29f58d93119eccccc4012c12935"
         )
     ]
 )
